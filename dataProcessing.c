c98c45fce37a05bb2dda8ec224edce12e8e4d14d
andrew <asc269@cornell.edu>
2023-05-10 23:24:33 -0400

    bug fix on dataProcessing

--- dataProcessing.c
+++ dataProcessing.c
@@ -7,6 +7,44 @@
 #define FILTER_SIZE 5
 #define INTENSITY_DIFFERENCE_THRESHOLD 1
 
+// Gives maximum of an input array of size [size]
+double max(double input[], int size)
+{
+  if (size <= 0)
+  {
+    return 0;
+  }
+
+  int maximum = input[0];
+  for (int i = 1; i < size; i++)
+  {
+    if (input[i] > maximum)
+    {
+      maximum = input[i];
+    }
+  }
+  return maximum;
+}
+
+// Gives minimum of an input array of size [size]
+double min(double input[], int size)
+{
+  if (size <= 0)
+  {
+    return 0;
+  }
+
+  int minimum = input[0];
+  for (int i = 1; i < size; i++)
+  {
+    if (input[i] < minimum)
+    {
+      minimum = input[i];
+    }
+  }
+  return minimum;
+}
+
 // Modifies [filter] to be a 1D Gaussian Filter
 void calculateGaussianFilter(int filter[])
 {
@@ -31,14 +69,16 @@ void applyGaussianFilter(int input[], int filter[], int size)
 
   // Apply the Gaussian filter
   for (int i = FILTER_SIZE / 2; i < size - FILTER_SIZE / 2; i++)
-  {
-    sum = 0.0;
-
-    for (int j = 0; j < FILTER_SIZE; j++)
-      sum += input[i + j - FILTER_SIZE / 2] * filter[j];
-
-    input[i] = sum;
-  }
+    for (int i = 0; i < size; i++)
+    {
+      sum = 0.0;
+      for (int j = 0; j < FILTER_SIZE; j++)
+      {
+        int index = (i + j - FILTER_SIZE / 2 + size) % size; // wrap around
+        sum += input[index] * filter[j];
+      }
+      input[i] = sum;
+    }
 
   // Find the maximum and minimum values in the filtered data
   double min_val = min(input, size);
@@ -56,10 +96,10 @@ int exceedDifferenceThreshold(int input[], int size)
   int filter[FILTER_SIZE];
   calculateGaussianFilter(filter);
 
-  int inputCopy[size];
+  double inputCopy[size];
   for (int i = 0; i < size; i++)
   {
-    inputCopy[i] = input[i];
+    inputCopy[i] = (double)input[i];
   }
 
   applyGaussianFilter(inputCopy, filter, size);
@@ -67,42 +107,4 @@ int exceedDifferenceThreshold(int input[], int size)
   if (max(inputCopy, size) - min(inputCopy, size) > INTENSITY_DIFFERENCE_THRESHOLD)
     return 1;
   return 0;
-}
-
-// Gives maximum of an input array of size [size]
-int max(int input[], int size)
-{
-  if (size <= 0)
-  {
-    return 0;
-  }
-
-  int maximum = input[0];
-  for (int i = 1; i < size; i++)
-  {
-    if (input[i] > maximum)
-    {
-      maximum = input[i];
-    }
-  }
-  return maximum;
-}
-
-// Gives minimum of an input array of size [size]
-int min(int input[], int size)
-{
-  if (size <= 0)
-  {
-    return 0;
-  }
-
-  int minimum = input[0];
-  for (int i = 1; i < size; i++)
-  {
-    if (input[i] < minimum)
-    {
-      minimum = input[i];
-    }
-  }
-  return minimum;
 }
